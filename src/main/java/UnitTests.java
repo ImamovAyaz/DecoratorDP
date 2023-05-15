714bfea8aefb96a2c20f07698f15a32f21f3507d
Imamov Ayaz <ayaz.imamov.99@mail.ru>
2023-05-15 20:15:42 +0300

    Reflection

--- src/main/java/UnitTests.java
+++ src/main/java/UnitTests.java
@@ -3,8 +3,14 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Method;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class UnitTests {
+    RarReaderDecorator rarReaderDecorator = new RarReaderDecorator(null);
+
     @DataProvider
     public Object[][] decoratorProviderMethod() {
         return new Object[][]{
@@ -12,6 +18,16 @@ public class UnitTests {
                 {new ZipReaderDecorator(null)},
                 {new MD5EncryptReaderDecorator(null)},
                 {new RSAEncryptReaderDecorator(null)}
+        };
+    }
+
+    @DataProvider
+    public Object[][] objectAndStreamProvider() {
+        return new Object[][]{
+                {new RarReaderDecorator(null), "unArch"},
+                {new ZipReaderDecorator(null), "unArch"},
+                {new MD5EncryptReaderDecorator(null), "encrypt"},
+                {new RSAEncryptReaderDecorator(null), "encrypt"}
         };
     }
 
@@ -34,6 +50,19 @@ public class UnitTests {
         } catch (Exception ex) {
             System.out.println(ex.toString());
         }
-        Assert.assertEquals("", readerStream.toString(), "Файл пустой");
+        assertEquals("", readerStream.toString(), "Файл пустой");
+    }
+
+    @Test(dataProvider = "objectAndStreamProvider")
+    public void unArchTest(ReaderDecorator readerProvider, String readerName) {
+        ByteArrayOutputStream readerStream = new ByteArrayOutputStream();
+        try {
+            Method stream = readerProvider.getClass().getDeclaredMethod(readerName, ByteArrayOutputStream.class);
+            stream.setAccessible(true);
+            String invoke = stream.invoke(readerProvider, readerStream).toString();
+            assertEquals(readerStream.toString(), invoke,"Разархивация/раскриптовка прошла успешно");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
